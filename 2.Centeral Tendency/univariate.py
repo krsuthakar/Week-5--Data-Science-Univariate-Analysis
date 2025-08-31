4ce9bc339d78daa0353558175887679294d17b01
Suthakar <suthakarstudy@gmail.com>
2025-08-31 18:09:36 +0530

    Add files via upload

---
+++ 2.Centeral Tendency/univariate.py
@@ -0,0 +1,14 @@
+class univariate():
+    def quanQual(dataset):
+        quan=[]
+        qual=[]
+        for columnName in dataset.columns:
+            #print(columnName)
+
+            if(dataset[columnName].dtype=='O'):
+                #print("qual")
+                qual.append(columnName)
+            else:
+                #print("quan")
+                quan.append(columnName)
+        return quan,qual 
